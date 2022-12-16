9209120b7604905bb485fe4b3a1de4b3128f45d3
Bektas Albayrak <bektas.albayrak@metocrm.com>
2022-12-16 16:10:24 +0300

    test for data from flutter

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@ export default {
     return {};
   },
   created() {
-    window.addEventListener("message", (data) => {
-      alert(data);
-      alert("after alert data");
-      alert("after alert data" + data);
-      print("vue-data: " + data);
-      console.log("vue-data: " + data);
-    });
-    window.addEventListener("message", () => {});
+    // window.addEventListener("message", (data) => {
+    //   alert(data);
+    //   alert("after alert data");
+    //   alert("after alert data" + data);
+    //   print("vue-data: " + data);
+    //   console.log("vue-data: " + data);
+    // });
+    // window.addEventListener("message", () => {});
   },
 };
 </script>
